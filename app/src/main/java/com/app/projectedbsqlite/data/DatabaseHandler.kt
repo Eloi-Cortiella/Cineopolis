8848460b7917c81e5174f7371eee63575b3eaa81
Eloi-Cortiella <eloicortiella@iesebre.com>
2025-10-18 17:19:18 +0200

    DatabaseHandler fet

--- app/src/main/java/com/app/projectedbsqlite/data/DatabaseHandler.kt
+++ app/src/main/java/com/app/projectedbsqlite/data/DatabaseHandler.kt
@@ -1,5 +1,19 @@
 package com.app.projectedbsqlite.data
 
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.app.projectedbsqlite.utils.Constants
 
-class DatabaseHandler() : SQLiteOpenHelper
+class DatabaseHandler(val context: Context) : SQLiteOpenHelper(context, Constants.DATABASE_NAME,null,Constants.DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(
+        db: SQLiteDatabase?,
+        oldVersion: Int,
+        newVersion: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+}
