2975afe1ba94b029f4e0111dfca75bacd6a8de97
Pablo-githubb <pablomaso@iesebre.com>
2025-10-18 17:23:02 +0200

    documentacio 2

--- app/src/main/java/com/app/projectedbsqlite/model/Pelicula.kt
+++ app/src/main/java/com/app/projectedbsqlite/model/Pelicula.kt
@@ -1,6 +1,10 @@
 package com.app.projectedbsqlite.model
 
-data class Pelicula(val id: Int, val titol: String, val any: Int, val director: String) {
+/**
+ * Classe que representa la nostra taula SQL amb les propietats de la pel·lícula.
+ * id, titol, any, director, valoració
+ */
+data class Pelicula(val id: Int, val titol: String, val any: Int, val director: String, val valoracio: Float) {
     // Constructor sense id per a noves pel·lícules
-    constructor(titol: String, any: Int, director: String) : this(-1, titol, any, director)
+    constructor(titol: String, any: Int, director: String, valoracio: Float) : this(-1, titol, any, director, valoracio)
 }
--- app/src/main/java/com/app/projectedbsqlite/utils/Constants.kt
+++ app/src/main/java/com/app/projectedbsqlite/utils/Constants.kt
@@ -1,5 +1,11 @@
 package com.app.projectedbsqlite.utils
 
+/**
+ * Classe objecte que utilitzem per guardar les constants de la taula, les constants representen les columnes de la taula.
+ * Dintre de la taula tenim les columnes: DATABASE_NAME (nom de la base de dades), DATABASE_VERSION (versió de la base de dades),
+ * TABLE_PELICULES (el nom de la taula), COLUM_ID (id de la taula), COLUMN_TITOL (titol de la taula), COLUMN_ANY (any de la pel·lícula),
+ * COLUMN_DIRECTOR (nom del director), COLUMN_VALORACIO (valoració de la pel·lícula).
+ */
 object Constants {
     const val DATABASE_NAME = "PeliculesDB"
     const val DATABASE_VERSION = 1
@@ -9,4 +15,5 @@ object Constants {
     const val COLUMN_TITOL = "titol"
     const val COLUMN_ANY = "any"
     const val COLUMN_DIRECTOR = "director"
+    const val COLUM_VALORACIO = "valoracio"
 }
